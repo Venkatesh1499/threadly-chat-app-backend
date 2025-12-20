f2b9489b44aaa028bd322b4a9379bca8252c2f0c
Venkatesh1499 <venkateshnimmalapudi100@gmail.com>
2025-12-20 02:39:58 -0800

    Added auhentication and login APIs

--- app.py
+++ app.py
@@ -1,9 +1,10 @@
 from flask import Flask, request, jsonify, Blueprint
+from Auth.routes import auth_bp
 import psycopg2
 
 app = Flask(__name__)
 
-
+app.register_blueprint(auth_bp)
 
 
 if __name__ == "__main__":

652685a4d356d2529caf66479877cf4ee141634c
Venkatesh1499 <venkateshnimmalapudi100@gmail.com>
2025-12-26 21:50:57 -0800

    Added API and table related logic for connection management

--- app.py
+++ app.py
@@ -2,12 +2,14 @@ from flask import Flask, request, jsonify, Blueprint
 import psycopg2
 from Auth.routes import auth_bp
 from UserRequests.routes import user_requests_db
+from ConnectionManagement.routes import connections_management_db
 
 app = Flask(__name__)
 
 # Registering blueprints
 app.register_blueprint(auth_bp)
 app.register_blueprint(user_requests_db)
+app.register_blueprint(connections_management_db)
 
 
 if __name__ == "__main__":
